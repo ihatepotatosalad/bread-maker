0743e1c864f211a2b86611a6850094a1239c3336
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-07-17 16:58:52 -0400

    c

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,4 +6,10 @@ breads.get('/', (req, res) => {
     res.send(Bread)
 
 })
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+})
+
 module.exports = breads
